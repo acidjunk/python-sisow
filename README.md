8220eaf33139d23a1b8bf4600a12ad6b649d7a7f
Nederhoed <r.r.nederhoed@gmail.com>
2012-10-19 02:14:20 +0200

    Warning in README

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ python-sisow
 ============
 
 Interface to Sisow, Dutch iDeal Payment Service Provider
+
+WARNING: This project has not really started yet.
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
-setup(name='mtgox',
-      version='1.0.2',
-      description="Interface to the MtGox Bitcoin trading platform",
-      author="Emery Hemingway",
-      author_email="emery@vfemail.net",
-      url='https://github.com/3M3RY/python-mtgox',
+setup(name='sisow',
+      version='0.1',
+      description="Interface to Sisow, Dutch iDeal Payment Service Provider",
+      author="Robert-Reinder Nederhoed",
+      author_email="r.r.nederhoed@gmail.com",
+      url='https://github.com/nederhoed/python-sisow',
       packages = ['mtgox']
       )
