68aec4df4d99cc8639ce50aeccac10ca7c6ebc5f
Nederhoed <r.r.nederhoed@gmail.com>
2012-10-20 23:08:30 +0200

    Preparation for adding python-sisow to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
-from distutils.core import setup
-setup(name='sisow',
-      version='0.1',
-      description="Interface to Sisow, Dutch iDeal Payment Service Provider",
-      author="Robert-Reinder Nederhoed",
-      author_email="r.r.nederhoed@gmail.com",
-      url='https://github.com/nederhoed/python-sisow',
-      packages = ['mtgox']
-      )
+"""\
+"""
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='sisow',
+    version='0.1',
+    description="Interface to Sisow, Dutch iDeal Payment Service Provider",
+    long_description=read('README.md'),
+    license="GPLv3",
+    author="Robert-Reinder Nederhoed",
+    author_email="r.r.nederhoed@gmail.com",
+    url='https://github.com/nederhoed/python-sisow',
+    packages=['mtgox', 'tests'],
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ]
+)
