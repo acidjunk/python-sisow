23124d09d11f8edeffc5781a7a9ce24506091a2e
Nederhoed <r.r.nederhoed@gmail.com>
2012-10-20 23:09:04 +0200

    second stage of implemenation. TransactionRequest is getting along.

--- sisow/__init__.py
+++ sisow/__init__.py
@@ -22,6 +22,7 @@ merchantkey=b36d8259346eaddb3c03236b37ad3a1d7a67cec6
 
 """
 from lxml import etree
+import hashlib
 
 import urllib
 import urllib2
@@ -36,11 +37,22 @@ def _xml_request(url, data=None):
     return etree.parse(stream)
 
 
+class WebshopURLs(object):
+    """\ """
+    def __init__(self, returnurl, cancelurl='', notifyurl='', callbackurl=''):
+        """Constructor """
+        self.returnurl = returnurl
+        self.cancelurl = cancelurl
+        self.notifyurl = notifyurl
+        self.callbackurl = callbackurl
+
+
 class Transaction(object):
     """\
     
     """
     def __init__(self, purchaseid, amount, issuerid, entrancecode, description=''):
+        """TODO: validation """
         self.shopid = ''
         self.payment = '' # Empty string indicates iDeal
         self.purchaseid = purchaseid
@@ -48,15 +60,20 @@ class Transaction(object):
         self.issuerid = issuerid
         self.entrancecode = entrancecode
         self.description = description
-        self.testmode = False
-        self.sha1 = None
         # Will be set in SisowAPI.start_transaction
-        self.merchantid = None
-        self.returnurl = ''
-        self.cancelurl = ''
-        self.notifyurl = ''
-        self.callbackurl = ''
+        self.merchantid = ''
+        self.testmode = False
     
+    def sha1(self, merchantkey):
+        """\
+        Return SHA1 value for:
+        purchaseid/entrancecode/amount/shopid/merchantid/merchantkey
+        """
+        key = "%(purchaseid)s%(entrancecode)s%(amount)s%(shopid)s%(merchantid)s"
+        sha1 = hashlib.sha1(key % self.__dict__)
+        sha1.update(merchantkey)
+        return sha1.hexdigest()
+        
     def __str__(self):
         return str(self.__dict__)
 
@@ -65,53 +82,124 @@ class SisowAPI(object):
     _url_api = 'https://www.sisow.nl/Sisow/iDeal/RestHandler.ashx/'
     _xmlns = 'https://www.sisow.nl/Sisow/REST'
     
-    def __init__(self, merchantid, merchantkey):
+    def __init__(self, merchantid, merchantkey, testmode=False):
         self.merchantid = merchantid
         self.merchantkey = merchantkey
-        
+        self._testmode = testmode
+    
     @property
-    def providers(self, test=False):
+    def providers(self):
         call = 'DirectoryRequest'
-        xml = _xml_request(self._url_api+call)
+        xml = _xml_request(self._url_api+call, data={'test': self._testmode})
         for issuer in xml.iter('{%s}issuer'%SisowAPI._xmlns):
             yield {'id': issuer[0].text, 'name': issuer[1].text}
     
-    def start_transaction(self, transaction, returnurl, cancelurl='',
-                          notifyurl='', callbackurl=''):
+    def start_transaction(self, transaction, urls):
         call = 'TransactionRequest'
         assert isinstance(transaction, Transaction)
+        # Testmode
+        transaction.testmode = 'true' if self._testmode else ''
+        # Add additional attributes
         transaction.merchantid = self.merchantid
-        transaction.returnurl = returnurl
-        transaction.cancelurl = cancelurl
-        transaction.notifyurl = notifyurl
-        transaction.callbackurl = callbackurl
-        xml = _xml_request(self._url_api+call) #, transaction.__dict__)
-        return etree.tostring(xml)
+        # Extract query parameters, a bit hacky
+        data = {}
+        data.update(transaction.__dict__)
+        data.update(urls.__dict__)
+        # SHA encoding!
+        data['sha1'] = transaction.sha1(self.merchantkey)
+        # Cleanup
+        for key in data.keys():
+            if data[key] is '':
+                del(data[key])
+        xml = _xml_request(self._url_api+call, data)
+        return TransactionResponse(xml)
 
-class SisowTestAPI(SisowAPI):
-    def __init__(self):
-        """Test API constructor """
-        key = 'b36d8259346eaddb3c03236b37ad3a1d7a67cec6'
-        super(SisowTestAPI, self).__init__('0123456', key)
+
+class Response(object):
+    """General response base class """
+    def __init__(self, xml):
+        """ """
+        self.sha1 = ''
+        self._signature = '' # Define in subclass
+        # Detect errorresponse
+        root = xml.getroot()
+        if root.tag == "{%s}errorresponse" % SisowAPI._xmlns:
+            raise ErrorResponse(xml)
+        # Process XML (second level)
+        offset = 2 + len(SisowAPI._xmlns)
+        for element in xml.findall(".//*/*"):
+            tag = element.tag[offset:]
+            if tag in self.__dict__:
+                self.__dict__[tag] = element.text
+            
+    def is_valid(self, merchantid, merchantkey):
+        """\
+        Validate the response using our merchantid and secret merchantkey
+        """
+        data = dict(merchantid=merchantid, merchantkey=merchantkey)
+        data.update(self.__dict__)
+        sha1 = hashlib.sha1(self._signature % data)
+        return sha1.hexdigest() == self.sha1
     
-    @property
-    def providers(self, test=False):
-        call = 'DirectoryRequest'
-        xml = _xml_request(self._url_api+call, data={'test': True})
-        for issuer in xml.iter('{%s}issuer'%SisowAPI._xmlns):
-            yield {'id': issuer[0].text, 'name': issuer[1].text}
+class TransactionResponse(Response):
+    """Specific transaction respons.
     
-    def start_transaction(self, transaction, returnurl, cancelurl='',
-                          notifyurl='', callbackurl=''):
-        transaction.testmode = True
-        super(SisowTestAPI, self).start_transaction(transaction, returnurl,
-                                                    cancelurl='', notifyurl='',
-                                                    callbackurl='')
+    <?xml version="1.0" encoding="UTF-8"?>
+    <transactionresponse xmlns="https://www.sisow.nl/Sisow/REST" version="1.0.0">
+        <transaction>
+            <issuerurl>IssuerURL</issuerurl>
+            <trxid>TransactionID</trxid>
+        </transaction>
+        <signature>
+            <sha1>SHA1 trxid + merchantid + merchantkey</sha1>
+        </signature>
+    </transactionresponse>
     
+    TODO: Sisow server is returning a transactionrequest!
+    """
+    def __init__(self, xml):
+        """Constructor  """
+        self.issuerurl = ''
+        self.trxid = ''
+        # Superclass
+        super(TransactionResponse, self).__init__(xml)
+        # Validation of type
+        root = xml.getroot()
+        # TODO: Sisow server is returning a transactionrequest!
+        if not root.tag == "{%s}transactionrequest" % SisowAPI._xmlns:  # transactionresponse
+            raise ValueError('Root node not "transactionrequest".\n\n'+etree.tostring(xml))
+        # Validation signature
+        # <sha1>SHA1 trxid + issuerurl + merchantid + merchantkey</sha1>
+        self._signature = "%(trxid)s%(issuerurl)s%(merchantid)s%(merchantkey)s"
+
+class ErrorResponse(Exception):
+    def __init__(self, xml):
+        # Extract info from XML
+        ns = SisowAPI._xmlns
+        code = xml.find(".//{%s}errorcode"%ns).text
+        message = xml.find(".//{%s}errormessage"%ns).text
+        super(ErrorResponse, self).__init__("%s %s"%(code, message))
+        self.code = code
+        self.message = message
+
+def sisow_account(filename):
+    with file(filename) as f:
+        return f.readline().strip(), f.readline().strip()
+
 if __name__ == "__main__":
-    api = SisowTestAPI()
-    #api = SisowAPI(1, 2)
-    #for i in api.providers:
-    #    print i
-    t = Transaction('test', 12301, 99, 'xtr123z', 'Bitcoins')
-    print api.start_transaction(t, 'http://www.bitbank.nl/yes/')
+    
+    (id_, key) = sisow_account('../account-sisow.secret')
+    api = SisowAPI(id_, key, False)
+    # Request banks
+    for i in api.providers:
+        print i
+    # Build transaction
+    t = Transaction('test', 1302, '09', 'xtr1234z', 'Blubber')
+    
+    #Send transaction
+    urls = WebshopURLs('http://test.nl/ok/')
+    result = api.start_transaction(t, urls)
+    print result.is_valid(id_, key)
+    print urllib.url2pathname(result.issuerurl)
+    
+    
